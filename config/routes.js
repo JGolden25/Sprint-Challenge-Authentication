f34d62f3d01939d9a599877dd6b3beab87255e6e
Joseph Chretien-Golden <joechre@gmail.com>
2018-10-26 11:57:07 -0500

    JWT -user credentials included

--- config/routes.js
+++ config/routes.js
@@ -14,10 +14,39 @@ module.exports = server => {
 
 function register(req, res) {
   const credentials = request.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 15);
+  credentials.password = hash;
+
+  db('users')
+  .insert(credentials)
+  .then(ids => {
+    const id = ids[0];
+    const token = jwt.sign({ username: credentials.username }, jwtKey, { expiresIn: '10m' });
+    response.status(201).json({ newUser: id, token  })
+  })
+  .catch( error => {
+    response.status(500).json(error);
+  })
 }
 
 function login(req, res) {
   const credentials = request.body;
+
+  db('users')
+  .where({ username: credentials.username })
+  .then(user => {
+    user = user[0]
+    if (user) {
+      const token = jwt.sign({ username: credentials.username }, jwtKey, { expiresIn: '10m' });
+      response.status(200).json({ welcome: user, token })
+    } else {
+      response.status(401).json({ notAuthorized: "Unable to find a user with the provided credentials." })
+    }
+  })
+  .catch ( error => {
+    response.status(500).json(error)
+  })
 }
 
 function getJokes(req, res) {
